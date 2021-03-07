3d221a6798aa64855ecd1f834936a1c077c1c705
intwaza <iintwazabelyse@gmail.com>
2021-03-07 09:29:12 +0300

    assignment1

---
+++ work.js
@@ -0,0 +1,8 @@
+function fruits() {
+    var fruit_list = ["mango", "tomato", "watermelon","pineaple","orange"]
+    fruit_list.forEach( element => console.log(element));
+ 
+    fruit_list[0];
+    console.log(fruit_list[0]);
+  }
+ fruits();
